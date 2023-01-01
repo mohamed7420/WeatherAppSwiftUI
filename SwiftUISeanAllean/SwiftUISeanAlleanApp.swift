8921db52564290e705a78837ea8b233b30a2bd68
MohamedOsama <Mohamedosama9878@gmail.com>
2023-01-01 13:23:23 +0200

    Initial Commit

---
+++ SwiftUISeanAllean/SwiftUISeanAlleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISeanAlleanApp.swift
+//  SwiftUISeanAllean
+//
+//  Created by MohamedOsama on 01/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISeanAlleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
